e1896a009b1f87b47bbc6358e819f19fb9ef27e5
Vlad <VladislavColpman@gmail.com>
2024-04-21 21:58:36 +1200

    Fixed it

--- Task181.m
+++ Task181.m
@@ -1,7 +1,7 @@
 clear;
 clc;
 
-for L = 86:100
+for L = 80:100
     l = findMinval(L);
 
     if l
@@ -22,29 +22,20 @@ fs = 8000;
 fcent = [697;770;852;941;1209;1336;1477;1633];
 
 % gets the responce
-[bb] = dtmfdesign(fcent, L, 8000);
+[bb, H, W] = dtmfdesign(fcent, (L), 8000);
 
-for i = 1:size(fcent, 1)
-    [H_i, W_i] = freqz(bb(:,i), 1, fs);
-
-    waosdoaksd = find(abs(H_i) > 0.25);
-    result = waosdoaksd(size(waosdoaksd,1)) + 1;
-
-    W_f = W_i .* (fs / (2 * pi));
-
-    meg(i) = W_i(result) * (fs / (2 * pi()));
-end
-
+i = 2;
 l = 1;
 
-for i = 1:(size(meg, 2) - 1)
+for j = 1:size(fcent, 1)
+    index = find(abs(H(:,j)) >= 0.25);
+    start = W(max(index),j);
 
-    if (meg(i) > (fcent(i + 1)))
+    if (start > ((fcent(i) / fs) * 2 * pi))
         l = 0;
         break;
     end
 
+    i = i + 1;
 end
-
-
 end
