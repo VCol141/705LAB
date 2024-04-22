8bb5d086a411147d374ff8dc97380657be6b392a
Vlad <VladislavColpman@gmail.com>
2024-04-22 14:20:53 +1200

    done

--- Task181.m
+++ Task181.m
@@ -1,8 +1,11 @@
 clear;
 clc;
 
+fs = 8000;
+fcent = [697;770;852;941;1209;1336;1477;1633];
+
 for L = 80:100
-    l = findMinval(L);
+    l = findMinval(L, fs, fcent);
 
     if l
         disp(L)
@@ -12,30 +15,37 @@ end
 
 close;
 
-findMinval(L);
+findMinval(L, fs, fcent);
 
+%% Basic Filter Test
+clear;
+clc;
 
-
-function l = findMinval(L)
-% System parameters
+L = 80;
 fs = 8000;
 fcent = [697;770;852;941;1209;1336;1477;1633];
 
-% gets the responce
-[bb, H, W] = dtmfdesign(fcent, (L), 8000);
+[bb, H, W] = dtmfdesign(fcent, L, fs);
 
-i = 2;
+
+%% Functions
+
+
+
+function l = findMinval(L, fs, fcent)
+% System parameters
+
+% gets the responce
+[bb, H, W] = dtmfdesign(fcent, L, fs);
 l = 1;
 
-for j = 1:size(fcent, 1)
+for j = 1:(size(fcent, 1) - 1)
     index = find(abs(H(:,j)) >= 0.25);
     start = W(max(index),j);
 
-    if (start > ((fcent(i) / fs) * 2 * pi))
+    if (start > ((fcent(j + 1) / fs) * 2 * pi))
         l = 0;
         break;
     end
-
-    i = i + 1;
 end
 end
