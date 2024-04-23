37372bce62921af7c74c8767e66d35e66fd586d8
Vlad <VladislavColpman@gmail.com>
2024-04-23 12:53:20 +1200

    Got it working well

---
+++ Task186.m
@@ -0,0 +1,50 @@
+clear;
+clc;
+
+fs = 8000;
+L = 86;
+fcent = [697;770;852;941;1209;1336;1477;1633];
+
+% b = load('unknown_number_22.mat');
+% a = b.xx;
+% 
+% a = dtmfdial(['1','2','3','4', '5', '6', '7', '8', '9', '0', '*', '#', 'A', 'B', 'C', 'D']);
+
+number = '01205978436';
+a = dtmfdial(number, 7);
+keys = dtmfrun(a,L,fs)
+
+%% Testing Accuracy
+
+
+k = 1000;
+f = 0;
+
+sum = 0;
+
+for i = 1:k
+    a = dtmfdial(number, 0);
+
+    a = bandpass(a, [697 1633], fs);
+
+    a1 = a * (1 / max(abs(a)));
+
+    for i = 1:size(a,2)
+        if abs(a1(i)) < 0.2
+            a(i) = 0;
+        end
+    end
+
+    keys = dtmfrun(a,L,fs);
+
+    for j = 1:size(keys,2)
+        if keys(j) == number(j)
+            sum = sum + 1;
+        end
+
+        f = f + 1;
+    end
+
+end
+
+sum / f
